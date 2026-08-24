5f6b1d30279013b2deae3a2f3f7c51f097a9eee2
Todd Judd <toddjudd@gmail.com>
2026-08-24 10:43:02 -0600

    chore: add 'managed by chezmoi' banner to dotfiles and templates

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
+# This file is managed by chezmoi (source: dotfiles repo). Edit it there and run
+# `chezmoi apply`, not here. See the chezmoi source dir for related config.
 op completion powershell | Out-String | Invoke-Expression
 Invoke-Expression (&starship init powershell)
